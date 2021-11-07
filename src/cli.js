48af71fbb747f6a06d573dbee9d1e9b63cca0590
Ignatiy Funtikov <ignatiysecond@gmail.com>
2021-11-07 22:18:46 +0300

    3 step

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+const toGreetUser = () => {
+    const nameOfUser = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${nameOfUser}!`);
+};
+
+export default toGreetUser;
