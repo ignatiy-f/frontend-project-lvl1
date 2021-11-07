cad24355112a0c59f5f1e3a8ca6c333383826927
Ignatiy Funtikov <ignatiysecond@gmail.com>
2021-11-08 00:58:16 +0300

    4 step

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import toGreetUser from './../src/cli.js';
+import toGreetUser from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 toGreetUser();
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const toGreetUser = () => {
-    const nameOfUser = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${nameOfUser}!`);
+  const nameOfUser = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${nameOfUser}!`);
 };
 
 export default toGreetUser;
